231529a95783f37d2506e52882d1aac46655b63a
manueloid <manuel.odelli@gmail.com>
2023-01-13 13:57:05 +0000

    Added the function robustness_all to evaluate all the different robustness

--- noise.jl
+++ noise.jl
@@ -1,19 +1,25 @@
-using Plots
 include("./src/ConstantQuantities.jl")
-## What I want is a fidelity that takes a control parameter and the control functions and spits out the  . 
-##
 f = ControlParameterFull(0.2, 40)
-tspan = range(0.1, 0.5, length=99) |> collect
-
-esta_rob = robustness_time(f, tspan)
-sta_rob = robustness_time(f, tspan; esta=false)
+i = ControlParameterInt(0.2, 40)
+tspan = range(0.1, 0.5, length=9) |> collect
+nlambda = 3
+robustness_time(f, tspan; esta = false) # Robustness of eSTA with the full Hamiltonian and hessian
 ##
-plot(tspan, esta_rob)
-plot!(tspan, sta_rob)
-
+using Plots
+function robustness_all(np::Int64, tspan::Vector{Float64}, Λf::Float64 = 50.0; nlambda::Int64 = 5, ω0::Float64=2.0   )
+    f = ControlParameterFull(ω0, 2.0√(Λf + 1.0), .2, np )
+    i = ControlParameterInt(ω0, 2.0√(Λf + 1.0), .2, np )
+    whole = [
+        robustness_time(f, tspan; nlambda = nlambda), # Robustness of eSTA with the full Hamiltonian and hessian
+        robustness_time(f, tspan;  nlambda = nlambda, hessian = false), # Robustness of eSTA with the full Hamilhonian and NO hessian
+        robustness_time(i, tspan;  nlambda = nlambda), # Robustness of eSTA with the intermediate Hamiltonian and hessian
+        robustness_time(i, tspan;  nlambda = nlambda, hessian = false), # Robustness of eSTA with the intermediate Hamilhonian and NO hessian
+        robustness_time(f, tspan; esta = false) # STA robustness ( as esta is set to false)
+    ]
+    return whole
+end
+test = robustness_all(50, tspan; nlambda = 2)
 ##
-fid_esta_hess = fidelity_time(f, tspan)
-fid_esta_no_hess = fidelity_time(f, tspan; hessian=false)
+plot(tspan , test, label = ["full" "full noh" "int" "int noh" "sta"])
 
-plot(tspan, fid_esta_hess)
-plot!(tspan, fid_esta_no_hess)
+
