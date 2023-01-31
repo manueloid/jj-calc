e975dd8b3ce6e5c3663420b4df3f402c24b5ddf6
manueloid <manuel.odelli@gmail.com>
2023-01-31 14:12:21 -0500

    Last commit before branching

---
+++ Documents/Notes/Chapters/Results/gfx/fidelityplot.jl
@@ -0,0 +1,18 @@
+# Array where the colors are defined and stored 
+colors = (
+    red=colorant"#FF0000",
+    yellow=colorant"#FFFF00",
+    blue=colorant"#0000FF",
+    green=colorant"#00FF00",
+    black=colorant"#000000"
+)
+
+# Array where all the line styles are stored
+styles = (
+    ldash="loosely dashed",
+    dash=" dashed",
+    solid="solid",
+    dot="dotted",
+    dot_dashed="dash pattern={on 4pt off 1pt on 1pt off 1pt}"
+)
+
--- calculations.jl
+++ calculations.jl
@@ -11,6 +11,23 @@ function fidelity_all(np::Int64, tspan::Vector{Float64}, Λf::Float64=50.0; nlam
     ]
     return whole
 end
+##
+# Data Saving
+##
+using DataFrames
+const data_folder = "./Documents/Notes/Chapters/Results/gfx/data/"
+filename(np::Int64) = "fidelity_np$np.dat"
+
+function fidelity_tofile(np::Int64)
+    try 
+        open(data_folder*filename(np))
+        println("this file exists")
+        close(data_folder*filename(np))
+    catch e
+        println("file does not exist")
+    end
+    
+end
 
 ##
 #Calculation and plotting
@@ -18,45 +35,3 @@ end
 tspan = range(0.04, 1.0, length=100) |> collect
 test = fidelity_all(50, tspan)
 
-##
-# Plotting stuff: I need style and colors, I will store them in two different arrays
-# I decided to use named tuples as they should give me finer control on the plotting options
-# but at the same type they give me the ability to loop through them if needed
-##
-using PGFPlotsX, Colors
-
-# Array where the colors are defined and stored 
-colors = (
-    red=colorant"#FF0000",
-    yellow=colorant"#FFFF00",
-    blue=colorant"#0000FF",
-    green=colorant"#00FF00",
-    black=colorant"#000000"
-)
-
-# Array where all the line styles are stored
-styles = (
-    ldash="loosely dashed",
-    dash=" dashed",
-    solid="solid",
-    dot="dotted",
-    dot_dashed="dash pattern={on 4pt off 1pt on 1pt off 1pt}"
-)
-
-for index in axes(styles, 1)
-    println(index)
-end
-
-
-ax = @pgf Axis()
-
-for (index, style) in enumerate(styles)
-    @pgf opt = {style = style, "thick", color = colors[index]}
-    push!(ax, @pgf Plot(opt, Table(tspan, test[index])))
-end
-ax
-
-@pgf opt = {style = styles.solid, "thick", color = colors.red}
-push!(ax, @pgf Plot(opt, Table(tspan, test[1])))
-@pgf opt = {style = styles.solid, "thick", color = colors.blue}
-push!(ax, @pgf Plot(opt, Table(tspan, test[5])))
