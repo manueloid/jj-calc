c7d9609530ca8139f1005de24719634ea1b90c25
manueloid <manuel.odelli@gmail.com>
2023-02-01 18:25:32 -0500

    Streamlined 'fidelityplot.jl and now plotting is much easier

--- Documents/Notes/Chapters/Results/gfx/fidelityplot.jl
+++ Documents/Notes/Chapters/Results/gfx/fidelityplot.jl
@@ -1,12 +1,10 @@
-##
 # Plot styles and features
-##
 
 # Array where the colors are defined and stored 
 using Colors, PGFPlotsX
 colors = (
     red=colorant"#FF0000",
-    yellow=colorant"#FFFF00",
+    yellow=colorant"#FFa000",
     blue=colorant"#0000FF",
     green=colorant"#00FF00",
     black=colorant"#000000"
@@ -17,13 +15,24 @@ styles = (
     ldash="loosely dashed",
     dash=" dashed",
     solid="solid",
-    dot="dotted",
-    dot_dashed="dash pattern={on 4pt off 1pt on 1pt off 1pt}"
+    dot="dash pattern={on 1pt off 1pt}",
+    dot_dash="dash pattern={on 4pt off 1pt on 1pt off 1pt}"
 )
-
-##
+opt = [@pgf {style = styles[index], color = colors[index], mark = "", thick} for index in 1:5]
 # Reading from file
-##
 using DataFrames, CSV
-filename(np::Int64, feature = "fidelity") = "./Documents/Notes/Chapters/Results/gfx/data/$(feature)_np$np.dat"
-data(np::Int64) = CSV.read(filename(np), DataFrame; header = true)
+filename(np::Int64, feature="fidelity") = "./Documents/Notes/Chapters/Results/gfx/data/$(feature)_np$np.dat"
+data(np::Int64, feature="fidelity") = CSV.read(filename(np, feature), DataFrame; header=true)
+df = data(30, "robustness")
+sort!(df, [:tf])
+pgfplot(data::DataFrame, options, type::Symbol) = @pgf Plot(options, Table(data.tf, data[!, type]))
+## Creating plots
+p1 = pgfplot(df, opt[2], :sta)
+p2 = @pgf PlotInc(opt[2], Table(df.tf, df.full))
+for opts in zip(styles, colors)
+
+end
+
+# Setting up axes and actual plottin
+ax = Axis([p1, p2])
+pgfsave("./Documents/Notes/Chapters/Results/gfx/test.pdf", ax)
