75fb769d997e03335501ba0b2a82105f2cd7d7f4
manueloid <manuel.odelli@gmail.com>
2023-02-04 00:59:20 -0500

    Major update on the plotting workflow
    - Created a new file 'PlotObj.jl' where I created two new types where I
      store the options for the plot
    - It then automatically saves the plots regarding both the robustness and
      the fidelity given a number of particles

---
+++ Documents/Notes/Chapters/Results/gfx/PlotObj.jl
@@ -0,0 +1,110 @@
+using DataFrames, CSV
+filename(np::Int64, feature="fidelity") = "./Documents/Notes/Chapters/Results/gfx/data/$(feature)_np$np.dat"
+data(np::Int64, feature="fidelity") = CSV.read(filename(np, feature), DataFrame; header=true)
+
+using Colors, PGFPlotsX
+# Colors for the plots
+colors = (
+    colorant"#FF0000", # eSTA Full Hamiltonian with Hessian 
+    colorant"#FFa000", # eSTA Full Hamiltonian with original version
+    colorant"#0000FF", # eSTA Intermediate Hamiltonian with Hessian
+    colorant"#00FF00",# eSTA Intermediate Hamiltonian original version
+    colorant"#000000" # STA
+)
+
+# Array where all the line styles are stored
+styles = (
+    solid="solid",                         # eSTA Intermediate Hamiltonian with Hessian
+    ldash="dash pattern={on 2pt off 2pt}",# eSTA Full Hamiltonian with Hessian 
+    dash=" dashed",                        # eSTA Full Hamiltonian with original version
+    dot="dash pattern={on 1pt off 1pt}",  # eSTA Intermediate Hamiltonian original version
+    dot_dash="dash pattern={on 4pt off 1pt on 1pt off 1pt}" #STA
+)
+
+names = ( 
+    "Full Hessian",
+    "Full Original",
+    "Intermediate Hessian",
+    "Intermediate Original",
+    "STA"
+)
+
+struct Options # temporary structure to hold the values of the Option
+    name::String
+    color::RGB
+    style::String
+end
+
+struct PlotObj
+    options::Options
+    fidelity::Vector{Float64}
+    robustness::Vector{Float64}
+    timespan::Vector{Float64}
+end
+
+# Assuming the two datasets have already been loaded?
+# I only want to pass the color and the style?
+# I think so. I will load the data outside the declaration of the PlotObj object and only then I will create it
+function PlotObj(options::Options, protocol::Symbol,fidelity::DataFrame, robustness::DataFrame )
+    return PlotObj(
+        options,
+        fidelity[!,protocol],
+        robustness[!,protocol],
+        robustness[!,:tf]
+    )
+end
+
+function PlotObj(options::Options, protocol::Symbol, np::Int64)
+    return PlotObj(
+        options,
+        data(np)[!,protocol],
+        data(np, "robustness")[!,protocol],
+        data(np)[!,:tf],
+    )
+end
+
+function PlotObj(np::Int64)
+    fidelities = data(np,"fidelity")
+    robustnesses = data(np,"robustness")
+    protocols = propertynames(fidelities)[2:end]
+    objects = []
+    for (index, protocol) in enumerate(protocols)
+        options = Options(names[index], colors[index], styles[index])
+        object = PlotObj(options, protocol, fidelities, robustnesses)
+        push!(objects, object)
+    end
+    return (; zip(protocols, objects)...)
+end
+
+whole = PlotObj(30)
+options(opt_obj::Options) = @pgf {color = opt_obj.color, style = opt_obj.style, thick}
+options(plt_obj::PlotObj) = options(plt_obj.options)
+
+plot_fidelity(plt_obj::PlotObj) = @pgf Plot(options(plt_obj), Table([plt_obj.timespan, plt_obj.fidelity]))
+plot_robustness(plt_obj::PlotObj) = @pgf Plot(options(plt_obj), Table([plt_obj.timespan, plt_obj.robustness]))
+legend(plt_obj::PlotObj) = @pgf LegendEntry(plt_obj.options.name)
+
+function export_fidelity(np::Int64, protocols::Vector = [:full, :full_orig, :interm, :interm_orig, :sta])
+    whole = PlotObj(np)
+    plots = []
+    legends = []
+    for symbols in protocols
+        push!(plots, plot_fidelity(whole[ symbols ]))
+        push!(legends, legend(whole[ symbols ]))
+    end
+    ax = @pgf Axis({title = "Fidelity $np particles"}, plots, legends)
+    return ax
+end
+
+function export_robustness(np::Int64, protocols::Vector = [:full, :full_orig, :interm, :interm_orig, :sta])
+    whole = PlotObj(np)
+    plots = []
+    legends = []
+    for symbols in protocols
+        push!(plots, plot_robustness(whole[ symbols ]))
+        push!(legends, legend(whole[ symbols ]))
+    end
+    ax = @pgf Axis({title = "Fidelity $np particles"}, plots, legends)
+    return ax
+end
+export_robustness(30)
--- Documents/Notes/Chapters/Results/gfx/fidelityplot.jl
+++ Documents/Notes/Chapters/Results/gfx/fidelityplot.jl
@@ -1,38 +1,8 @@
-# Plot styles and features
-
-# Array where the colors are defined and stored 
-using Colors, PGFPlotsX
-colors = (
-    red=colorant"#FF0000",
-    yellow=colorant"#FFa000",
-    blue=colorant"#0000FF",
-    green=colorant"#00FF00",
-    black=colorant"#000000"
-)
-
-# Array where all the line styles are stored
-styles = (
-    ldash="loosely dashed",
-    dash=" dashed",
-    solid="solid",
-    dot="dash pattern={on 1pt off 1pt}",
-    dot_dash="dash pattern={on 4pt off 1pt on 1pt off 1pt}"
-)
-opt = [@pgf {style = styles[index], color = colors[index], mark = "", thick} for index in 1:5]
-# Reading from file
-using DataFrames, CSV
-filename(np::Int64, feature="fidelity") = "./Documents/Notes/Chapters/Results/gfx/data/$(feature)_np$np.dat"
-data(np::Int64, feature="fidelity") = CSV.read(filename(np, feature), DataFrame; header=true)
-df = data(30, "robustness")
-sort!(df, [:tf])
-pgfplot(data::DataFrame, options, type::Symbol) = @pgf Plot(options, Table(data.tf, data[!, type]))
+gfplot(data::DataFrame, options, type::Symbol) = @pgf Plot(options, Table(data.tf, data[!, type]))
 ## Creating plots
-p1 = pgfplot(df, opt[2], :sta)
-p2 = @pgf PlotInc(opt[2], Table(df.tf, df.full))
-for opts in zip(styles, colors)
-
-end
-
+protocols = propertynames(df)[2:end]
+ax = Axis()
+[ push!(ax, pgfplot(df, opt[index], protocols[index])) for index in 1:5 ]
 # Setting up axes and actual plottin
-ax = Axis([p1, p2])
 pgfsave("./Documents/Notes/Chapters/Results/gfx/test.pdf", ax)
+run(`zathura ./Documents/Notes/Chapters/Results/gfx/test.pdf`)
