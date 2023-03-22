2affb65e95084b6421455227c1541a84ce6d8402
manueloid <manuel.odelli@gmail.com>
2023-03-22 22:28:20 +0000

    Started to calculate the number squeezing parameter in the squeezing.jl file.

---
+++ squeezing.jl
@@ -0,0 +1,14 @@
+#= Here I need to evaluate the number squeezing parameter, i.e. ξ² = ΔJz² \ (N\4) 
+What I want is a simulation of the Schrödinger equation that given the final time,
+the boundary conditions and the number of particles, calculates the corrections and then saved the desired quantity.
+I will only simulate the full Hessian and the STA Protocol to compare the two =#
+using JosephsonSTA
+include("src/ConstantQuantities.jl")
+Λ_sta(t::Float64, cp::ControlParameter) = control_ω(t, cp) * 0.25 - 1.0
+ω_esta(t::Float64, cp::ControlParameter, corrections::Vector{Float64}) = control_ω(t, cp) - correction_poly(t, cp, corrections::Vector{Float64})
+Λ_esta(t::Float64, cp::ControlParameter, corrections::Vector{Float64}) = ω_esta(t, cp, corrections) * 0.25 - 1.0
+
+f = ControlParameterFull(0.3, 30)
+q = ConstantQuantities(f)
+
+ξ(t, psi) = dagger(psi) * (q.Jz)^2 * psi - dagger(psi) * (q.Jz) * psi |> real
--- src/fidelities.jl
+++ src/fidelities.jl
@@ -9,79 +9,79 @@ fidelity(cp::ControlParameter, qts::ConstantQuantities, ω::Function) return the
 Useful to include it in other functions to iteratively calculate the fidelity
 """
 function fidelity(cp::ControlParameter, qts::ConstantQuantities, Λ::Function; hessian::Bool=true, nlambda::Int64=5, maxbra::Int64=4)
-    h = 2.0 / cp.NParticles
-    tf = cp.final_time
-    function H_eSTA(t, psi) # Function to return the time dependent Hamiltonian
-        return (h * Λ(t) * qts.Jz^2 - 2.0 * qts.Jx)
-    end
-    fidelity(t, psi) = abs2.(dagger(qts.ψf) * psi)
-    return timeevolution.schroedinger_dynamic([0.0, tf], qts.ψ0, H_eSTA; fout=fidelity)[2][end]  # Time evolution where the output is not the resulting state but the fidelity. It helps improving the speed of the calculation
+        h = 2.0 / cp.NParticles
+        tf = cp.final_time
+        function H_eSTA(t, psi) # Function to return the time dependent Hamiltonian
+                return (h * Λ(t) * qts.Jz^2 - 2.0 * qts.Jx)
+        end
+        fidelity(t, psi) = abs2.(dagger(qts.ψf) * psi)
+        return timeevolution.schroedinger_dynamic([0.0, tf], qts.ψ0, H_eSTA; fout=fidelity)[2][end]  # Time evolution where the output is not the resulting state but the fidelity. It helps improving the speed of the calculation
 end
 
 function fidelity_time_esta(cp::ControlParameter, tarr::Vector{Float64}; hessian::Bool=true, nlambda::Int64=5, maxbra::Int64=4)
-    qts = ConstantQuantities(cp)
-    fidelity_array = zeros(length(tarr))
-    Threads.@threads for (index, tf) in enumerate(tarr) |> collect
-        # for (index, tf) in enumerate(tarr) |> collect
-        cparam = cp_time(cp, tf)
-        corrs = corrections(cparam; hessian=hessian, nlambda=nlambda, maxbra=maxbra)
-        ω(t) = ω_esta(t, cparam, corrs)
-        function H_eSTA(t, psi) # Function to return the time dependent Hamiltonian
-            return (2.0 / cparam.NParticles * (ω(t) / 4.0 - 1.0) * qts.Jz^2 - 2.0 * qts.Jx)
+        qts = ConstantQuantities(cp)
+        fidelity_array = zeros(length(tarr))
+        Threads.@threads for (index, tf) in enumerate(tarr) |> collect
+                # for (index, tf) in enumerate(tarr) |> collect
+                cparam = cp_time(cp, tf)
+                corrs = corrections(cparam; hessian=hessian, nlambda=nlambda, maxbra=maxbra)
+                ω(t) = ω_esta(t, cparam, corrs)
+                function H_eSTA(t, psi) # Function to return the time dependent Hamiltonian
+                        return (2.0 / cparam.NParticles * (ω(t) / 4.0 - 1.0) * qts.Jz^2 - 2.0 * qts.Jx)
+                end
+                fidelity(t, psi) = abs2.(dagger(qts.ψf) * psi)
+                fidelity_array[index] = timeevolution.schroedinger_dynamic([0.0, tf], qts.ψ0, H_eSTA; fout=fidelity)[2][end]  # Time evolution where the output is not the resulting state but the fidelity. It helps improving the speed of the calculation
+                println("Calculating fidelity for final time $tf ")
         end
-        fidelity(t, psi) = abs2.(dagger(qts.ψf) * psi)
-        fidelity_array[index] = timeevolution.schroedinger_dynamic([0.0, tf], qts.ψ0, H_eSTA; fout=fidelity)[2][end]  # Time evolution where the output is not the resulting state but the fidelity. It helps improving the speed of the calculation
-        println("Calculating fidelity for final time $tf ")
-    end
-    return fidelity_array
+        return fidelity_array
 end
 
 function fidelity_time_sta(cp::ControlParameter, tarr::Vector{Float64}; hessian::Bool=true, nlambda::Int64=5, maxbra::Int64=4)
-    qts = ConstantQuantities(cp)
-    fidelity_array = zeros(length(tarr))
-    Threads.@threads for (index, tf) in enumerate(tarr) |> collect
-        # for (index, tf) in enumerate(tarr) |> collect
-        cparam = cp_time(cp, tf)
-        ω(t) = control_ω(t, cparam)
-        function H_eSTA(t, psi) # Function to return the time dependent Hamiltonian
-            return (2.0 / cparam.NParticles * (ω(t) / 4.0 - 1.0) * qts.Jz^2 - 2.0 * qts.Jx)
+        qts = ConstantQuantities(cp)
+        fidelity_array = zeros(length(tarr))
+        Threads.@threads for (index, tf) in enumerate(tarr) |> collect
+                # for (index, tf) in enumerate(tarr) |> collect
+                cparam = cp_time(cp, tf)
+                ω(t) = control_ω(t, cparam)
+                function H_eSTA(t, psi) # Function to return the time dependent Hamiltonian
+                        return (2.0 / cparam.NParticles * (ω(t) / 4.0 - 1.0) * qts.Jz^2 - 2.0 * qts.Jx)
+                end
+                fidelity(t, psi) = abs2.(dagger(qts.ψf) * psi)
+                fidelity_array[index] = timeevolution.schroedinger_dynamic([0.0, tf], qts.ψ0, H_eSTA; fout=fidelity)[2][end]  # Time evolution where the output is not the resulting state but the fidelity. It helps improving the speed of the calculation
+                println("Calculating fidelity for final time $tf ")
         end
-        fidelity(t, psi) = abs2.(dagger(qts.ψf) * psi)
-        fidelity_array[index] = timeevolution.schroedinger_dynamic([0.0, tf], qts.ψ0, H_eSTA; fout=fidelity)[2][end]  # Time evolution where the output is not the resulting state but the fidelity. It helps improving the speed of the calculation
-        println("Calculating fidelity for final time $tf ")
-    end
-    return fidelity_array
+        return fidelity_array
 end
 
 function fidelity_time(cp::ControlParameter, tarr::Vector{Float64}; esta::Bool=true, hessian::Bool=true, nlambda::Int64=5, maxbra::Int64=4)
-    if esta == true
-        return fidelity_time_esta(cp, tarr; hessian=hessian, nlambda=nlambda, maxbra=maxbra)
-    else
-        return fidelity_time_sta(cp, tarr)
-    end
+        if esta == true
+                return fidelity_time_esta(cp, tarr; hessian=hessian, nlambda=nlambda, maxbra=maxbra)
+        else
+                return fidelity_time_sta(cp, tarr)
+        end
 end
 
 function fidelity_search(cp::ControlParameter, epsilons::Vector{Float64}; hessian::Bool=true, nlambda::Int64=5, maxbra::Int64=4)
-    corrs = corrections(cp; hessian=hessian, nlambda=nlambda, maxbra=maxbra)
-    qts = ConstantQuantities(cp)
-    fidelity_array = zeros(length(epsilons))
-    Threads.@threads for (index, ϵ) in enumerate(epsilons) |> collect
-        # for (index, ϵ) in enumerate(epsilons) |> collect
-        ω(t) = ω_esta(t, cp, ϵ * corrs)
-        function H_eSTA(t, psi) # Function to return the time dependent Hamiltonian
-            return (2.0 / cp.NParticles * (ω(t) / 4.0 - 1.0) * qts.Jz^2 - 2.0 * qts.Jx)
+        corrs = corrections(cp; hessian=hessian, nlambda=nlambda, maxbra=maxbra)
+        qts = ConstantQuantities(cp)
+        fidelity_array = zeros(length(epsilons))
+        Threads.@threads for (index, ϵ) in enumerate(epsilons) |> collect
+                # for (index, ϵ) in enumerate(epsilons) |> collect
+                ω(t) = ω_esta(t, cp, ϵ * corrs)
+                function H_eSTA(t, psi) # Function to return the time dependent Hamiltonian
+                        return (2.0 / cp.NParticles * (ω(t) / 4.0 - 1.0) * qts.Jz^2 - 2.0 * qts.Jx)
+                end
+                fidelity(t, psi) = abs2.(dagger(qts.ψf) * psi)
+                println("Calculating fidelity for ϵ $ϵ")
+                fidelity_array[index] = timeevolution.schroedinger_dynamic([0.0, cp.final_time], qts.ψ0, H_eSTA; fout=fidelity)[2][end]  # Time evolution where the output is not the resulting state but the fidelity. It helps improving the speed of the calculation
         end
-        fidelity(t, psi) = abs2.(dagger(qts.ψf) * psi)
-        println("Calculating fidelity for ϵ $ϵ")
-        fidelity_array[index] = timeevolution.schroedinger_dynamic([0.0, cp.final_time], qts.ψ0, H_eSTA; fout=fidelity)[2][end]  # Time evolution where the output is not the resulting state but the fidelity. It helps improving the speed of the calculation
-    end
-    return fidelity_array
+        return fidelity_array
 end
 
 using FiniteDifferences
 """
 `robustness(cp::ControlParameter, qts::ConstantQuantities, ω::Function; order::Int64=5)`
-return the derivative of the fidelity given an error of the form 1 + δ for δ = 0
+return the derivative of the fidelity given an error in the modulation of the control parameter of the form Λδ(t) = (1 + δ)Λ(t)
 # Arguments 
 - `cp` 
 - `qts` 
@@ -90,43 +90,61 @@ return the derivative of the fidelity given an error of the form 1 + δ for δ =
 - `order` is the order of the finite difference method I need to use
 """
 function robustness(cp::ControlParameter, qts::ConstantQuantities, Λ::Function; order::Int64=5)
-    function error(δ)
-        Λ_err(t) = (1 + δ) * Λ(t)
-        return fidelity(cp, qts, Λ_err)
-    end
-    m = central_fdm(order, 1)
-    return m(error, 0.0) |> abs
+        function error(δ)
+                Λ_err(t) = (1 + δ) * Λ(t)
+                return fidelity(cp, qts, Λ_err)
+        end
+        m = central_fdm(order, 1)
+        return m(error, 0.0) |> abs
+end
+"""
+`robustness_time(cp::ControlParameter, qts::ConstantQuantities, ω::Function; order::Int64=5)`
+return the derivative of the fidelity given an offset in the time of the control parameter of the form Λδ(t) = Λ(t + δ)
+# Arguments 
+- `cp` 
+- `qts` 
+- `Λ` must be a function of only one parameter (ideally the time) and it has to be built outside the function declaration 
+# Kwargs 
+- `order` is the order of the finite difference method I need to use
+"""
+function robustness(cp::ControlParameter, qts::ConstantQuantities, Λ::Function; order::Int64=5)
+        function error(δ)
+                Λ_err(t) = Λ(δ + t)
+                return fidelity(cp, qts, Λ_err)
+        end
+        m = central_fdm(order, 1)
+        return m(error, 0.0) |> abs
 end
 
 function robustness_time_esta(cp::ControlParameter, tarr::Vector{Float64}; nlambda::Int64=5, hessian::Bool=true)
-    qts = ConstantQuantities(cp)
-    robustness_arr = zeros(length(tarr))
-    Threads.@threads for (index, tf) in enumerate(tarr) |> collect
-        cparam = cp_time(cp, tf)
-        corrs = corrections(cparam; hessian=hessian, nlambda=nlambda)
-        esta(t) = Λ_esta(t, cparam, corrs)
-        println("Calculating robustness for time $tf")
-        robustness_arr[index] = robustness(cparam, qts, esta)
-    end
-    return robustness_arr
+        qts = ConstantQuantities(cp)
+        robustness_arr = zeros(length(tarr))
+        Threads.@threads for (index, tf) in enumerate(tarr) |> collect
+                cparam = cp_time(cp, tf)
+                corrs = corrections(cparam; hessian=hessian, nlambda=nlambda)
+                esta(t) = Λ_esta(t, cparam, corrs)
+                println("Calculating robustness for time $tf")
+                robustness_arr[index] = robustness(cparam, qts, esta)
+        end
+        return robustness_arr
 end
 
 function robustness_time_sta(cp::ControlParameter, tarr::Vector{Float64})
-    qts = ConstantQuantities(cp)
-    robustness_arr = zeros(length(tarr))
-    Threads.@threads for (index, tf) in enumerate(tarr) |> collect
-        cparam = cp_time(cp, tf)
-        sta(t) = Λ_sta(t, cparam)
-        println("Calculating robustness for time $tf")
-        robustness_arr[index] = robustness(cparam, qts, sta)
-    end
-    return robustness_arr
+        qts = ConstantQuantities(cp)
+        robustness_arr = zeros(length(tarr))
+        Threads.@threads for (index, tf) in enumerate(tarr) |> collect
+                cparam = cp_time(cp, tf)
+                sta(t) = Λ_sta(t, cparam)
+                println("Calculating robustness for time $tf")
+                robustness_arr[index] = robustness(cparam, qts, sta)
+        end
+        return robustness_arr
 end
 
 function robustness_time(cp::ControlParameter, tarr::Vector{Float64}; nlambda::Int64=5, hessian::Bool=true, esta::Bool=true)
-    if esta == true
-        return robustness_time_esta(cp, tarr; nlambda=nlambda, hessian=hessian)
-    else
-        return robustness_time_sta(cp, tarr)
-    end
+        if esta == true
+                return robustness_time_esta(cp, tarr; nlambda=nlambda, hessian=hessian)
+        else
+                return robustness_time_sta(cp, tarr)
+        end
 end
