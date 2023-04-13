84b3ee5565b58262e68bcdf276cd1f8fc4d6de69
manueloid <manuel.odelli@gmail.com>
2023-04-13 07:10:40 +0100

    Improved noise and time noise
    Streamlined the time noise functions and better documented the noise functions.

--- Noise/noise.jl
+++ Noise/noise.jl
@@ -1,4 +1,4 @@
-include("./src/ConstantQuantities.jl")
+include("../src/ConstantQuantities.jl")
 f = ControlParameterFull(0.2, 40)
 i = ControlParameterInt(0.2, 40)
 tspan = range(0.1, 0.5, length=9) |> collect
@@ -7,19 +7,17 @@ robustness_time(f, tspan; esta=false) # Robustness of eSTA with the full Hamilto
 ##
 using Plots
 function robustness_all(np::Int64, tspan::Vector{Float64}, Λf::Float64=50.0; nlambda::Int64=5, ω0::Float64=2.0)
-        f = ControlParameterFull(ω0, 2.0√(Λf + 1.0), 0.2, np)
-        i = ControlParameterInt(ω0, 2.0√(Λf + 1.0), 0.2, np)
-        whole = [
-                robustness_time(f, tspan; nlambda=nlambda), # Robustness of eSTA with the full Hamiltonian and hessian
-                robustness_time(f, tspan; nlambda=nlambda, hessian=false), # Robustness of eSTA with the full Hamilhonian and NO hessian
-                robustness_time(i, tspan; nlambda=nlambda), # Robustness of eSTA with the intermediate Hamiltonian and hessian
-                robustness_time(i, tspan; nlambda=nlambda, hessian=false), # Robustness of eSTA with the intermediate Hamilhonian and NO hessian
-                robustness_time(f, tspan; esta=false) # STA robustness ( as esta is set to false)
-        ]
-        return whole
+    f = ControlParameterFull(ω0, 2.0√(Λf + 1.0), 0.2, np)
+    i = ControlParameterInt(ω0, 2.0√(Λf + 1.0), 0.2, np)
+    whole = [
+        robustness_time(f, tspan; nlambda=nlambda), # Robustness of eSTA with the full Hamiltonian and hessian
+        robustness_time(f, tspan; nlambda=nlambda, hessian=false), # Robustness of eSTA with the full Hamilhonian and NO hessian
+        robustness_time(i, tspan; nlambda=nlambda), # Robustness of eSTA with the intermediate Hamiltonian and hessian
+        robustness_time(i, tspan; nlambda=nlambda, hessian=false), # Robustness of eSTA with the intermediate Hamilhonian and NO hessian
+        robustness_time(f, tspan; esta=false) # STA robustness ( as esta is set to false)
+    ]
+    return whole
 end
 test = robustness_all(50, tspan; nlambda=2)
 ##
 plot(tspan, test, label=["full" "full noh" "int" "int noh" "sta"])
-
-
--- Noise/timenoise.jl
+++ Noise/timenoise.jl
@@ -1,55 +1,49 @@
-include("src/ConstantQuantities.jl")
-
+include("../src/ConstantQuantities.jl")
+"""
+robustness_timenoise(cp::ControlParameter, qts::ConstantQuantities, Λ::Function; order::Int64=5) -> Float64
+Calculates the robustness of the control parameter cp to time noise, using the function Λ.
+It does this by calculating the derivative of the fidelity with respect to the time noise δ, and then calculating the absolute value of that derivative.
+It does this by using the central finite difference method, with order `order`.
+"""
 function robustness_timenoise(cp::ControlParameter, qts::ConstantQuantities, Λ::Function; order::Int64=5)
-        function error(δ)
-                Λ_err(t) = Λ(δ + t)
-                return fidelity(cp, qts, Λ_err)
-        end
-        m = central_fdm(order, 1)
-        return m(error, 0.0) |> abs
+    function error(δ) # Evaluate the fidelity with shift δ in time
+        Λ_err(t) = Λ(δ + t)
+        return fidelity(cp, qts, Λ_err)
+    end
+    m = central_fdm(order, 1) # Create a central finite difference method
+    return m(error, 0.0) |> abs # Calculate the derivative of the fidelity with respect to δ, and take the absolute value
 end
-function robustness_timenoise_esta(cp::ControlParameter, tarr::Vector{Float64}; nlambda::Int64=5, hessian::Bool=true)
-        qts = ConstantQuantities(cp)
-        robustness_arr = zeros(length(tarr))
-        Threads.@threads for (index, tf) in enumerate(tarr) |> collect
-                cparam = cp_time(cp, tf)
-                corrs = corrections(cparam; hessian=hessian, nlambda=nlambda)
-                esta(t) = Λ_esta(t, cparam, corrs)
-                println("Calculating robustness for time $tf")
-                robustness_arr[index] = robustness_timenoise(cparam, qts, esta)
-        end
-        return robustness_arr
+"""
+robustnesses_time(cp::ControlParameter; t::Vector{Float64}; nlambda::Int64=5) -> Vector{Float64}
+Evaluates the robustness of the control parameter cp to time noise, using the eSTA corrections and the STA corrections.
+"""
+function robustnesses_time(cp::ControlParameter, final_times; nlambda::Int64=5)
+	qts = ConstantQuantities(cp)
+	robustness_sta = zeros(length(final_times))
+	robustness_esta = zeros(length(final_times))
+	Threads.@threads for (index, tf) in enumerate(final_times) |> collect
+		cparam = cp_time(cp, tf)
+		corrs = corrections(cparam; nlambda=nlambda)
+		esta(t) = Λ_esta(t, cparam, corrs)
+		println("Calculating robustness for time $tf, eSTA")
+		sta(t) = Λ_sta(t, cparam)
+		println("Calculating robustness for time $tf, STA")
+		robustness_sta[index] = robustness_timenoise(cparam, qts, sta)
+		robustness_esta[index] = robustness_timenoise(cparam, qts, esta)
+	end
+	return robustness_sta, robustness_esta
 end
-function robustness_timenoise_sta(cp::ControlParameter, tarr::Vector{Float64})
-        qts = ConstantQuantities(cp)
-        robustness_arr = zeros(length(tarr))
-        Threads.@threads for (index, tf) in enumerate(tarr) |> collect
-                cparam = cp_time(cp, tf)
-                sta(t) = Λ_sta(t, cparam)
-                println("Calculating robustness for time $tf")
-                robustness_arr[index] = robustness_timenoise(cparam, qts, sta)
-        end
-        return robustness_arr
-end
-function robustness_timenoise(cp::ControlParameter, tarr::Vector{Float64}; nlambda::Int64=5, hessian::Bool=true, esta::Bool=true)
-        if esta == true
-                return robustness_timenoise_esta(cp, tarr; nlambda=nlambda, hessian=hessian)
-        else
-                return robustness_timenoise_sta(cp, tarr)
-        end
+"""
+robustnesses_time(n::Int64, final_times; nlambda::Int64=5) -> Vector{Float64}
+Given a number of particles n, evaluates the robustness for both the STA and eSTA corrections, for a range of times.
+"""
+function robustnesses_time(n::Int64, final_times; nlambda::Int64=5)
+	cp = ControlParameterFull(0.2, n)
+	return robustnesses_time(cp, final_times; nlambda=nlambda)
 end
 
-cp = ControlParameterFull(.2,40)
-tspan = range(0.2,0.85, length = 100)|> collect
-sta = robustness_timenoise(cp, tspan; esta = false)
-esta = robustness_timenoise(cp, tspan)
+tspan = range(0.2, 0.85, length=100) |> collect
+sta, esta = robustnesses_time(cp, tspan)
 using Plots
-plot(tspan, esta, label = "eSTA")  
-plot!(tspan, sta, label = "STA")  
-
-cp = ControlParameterFull(.2,40)
-qts = ConstantQuantities(cp)
-corrs = corrections(cp) 
-Λ(t)=Λ_esta(t+.001, cp, corrs) 
-
-fidelity(cp, qts, Λ)
+plot(tspan, esta, label="eSTA")
+plot!(tspan, sta, label="STA")
